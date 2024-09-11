334b6f9e6f38ca48b8c6c09c928263942c021e10
Elie <elie.c.khoure@gmail.com>
2024-09-11 19:10:23 +0300

    Final changes and tests

--- frontend/todo-app/src/app/services/todo.service.ts
+++ frontend/todo-app/src/app/services/todo.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ITODO } from '../models/todo.model';
 
@@ -7,17 +7,26 @@ import { ITODO } from '../models/todo.model';
 })
 export class TodoService {
 
+  private apiUrl = 'http://localhost:5080/api/Todo';
+
   constructor(private http: HttpClient) { }
 
-  getToDos(){
+  getToDos() {
     return this.http.get<ITODO[]>('http://localhost:5080/api/Todo');
   }
 
-  addTodo(content: string){
-    return this.http.post<ITODO>('http://localhost:5080/api/Todo', content);
+  addTodo(content: string) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    // Ensure the content is sent as a JSON string
+    const body = JSON.stringify({ content });
+
+    return this.http.post<ITODO>(this.apiUrl, body, { headers });
   }
 
-  deleteTodo(id: number){
+  deleteTodo(id: number) {
     return this.http.delete<ITODO>(`http://localhost:5080/api/Todo/${id}`);
   }
 }
